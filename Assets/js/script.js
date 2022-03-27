19523791ae5a8c9cfac8ed5fde71cdce5dba7b95
Laura Antunez <lauraantunez024@gmail.com>
2022-03-27 01:48:34 -0400

    populated questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,230 +1,259 @@
-startBtn = document.querySelector('#start')
-playAgainBtn = document.querySelector('#play-again')
-gameOverScrn = document.querySelector('#GameOver')
-countdown = document.querySelector('#countdown')
-info_box = document.querySelector('.info_box')
-youLost = document.querySelector('#youlost')
-
-
-
-
-
-
-
-
 // Questions/ Answers
 
-
-// var questions = [
-//     "On what platform did the original Kingdom Hearts game come out on?",
-//     "What video game company made Kingdom Hearts?",
-//     "Who is Sora's nobody?",
-//     "What does Riku want to name the boat in the first game?",
-//     "In what world does Sora meet Donald and Goofy the first time?",
-//     "Who is 'the king?'",
-//     "What is Hollow Bastion's original name?",
-//     "Who turns into a heartless in the first game?",
-//     "On what platform did the second game, Chain of Memories, come out?",
-//     "Who is the main villain in Kingdom Hearts?"
-// ]
-
-let questions = [
+let quiz = [
     {
-    numb: 1,
-    question: "On what platform did the original Kingdom Hearts game come out on?",
+    question: "On what Platform did the original Kingdom Hearts game come out on?",
     answer: "PlayStation",
-    options: [
-      "GameBoy",
-      "PlayStation",
-      "Super Nintendo",
-      "Nintendo 64"
-    ]
+    optionA:"GameBoy",
+    optionB:"PlayStation",
+    optionC:"Super Nintendo",
+    optionD:"Nintendo 64"
+    
   },
     {
-    numb: 2,
     question: "What video game company made Kingdom Hearts?",
     answer: "Square Enix",
-    options: [
-      "Sega",
-      "Bandai/Namco",
-      "Konami",
-      "Square Enix"
-    ]
+    optionA: "Sega",
+    optionB: "Bandai/Namco",
+    optionC: "Konami",
+    optionD: "Square Enix"
+    
   },
     {
-    numb: 3,
     question: "Who is Sora's nobody?",
     answer: "Roxas",
-    options: [
-      "Roxas",
-      "Riku",
-      "Kairi",
-      "Xehanort"
-    ]
+    
+    optionA:"Roxas",
+    optionB:"Riku",
+    optionC:"Kairi",
+    optionD:"Xehanort"
+    
   },
     {
-    numb: 4,
     question: "What does Riku want to name the boat in the first game?",
     answer: "High Wind",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "High Wind",
-      "Structured Query Language"
-    ]
+    optionA: "Stylish Question Language",
+    optionB:"Stylesheet Query Language",
+    optionC:"High Wind",
+    optionD:"Structured Query Language"
+    
   },
-    {
-    numb: 5,
+  {
     question: "In what world does Sora meet Donald and Goofy the first time?",
     answer: "Traverse Town",
-    options: [
-      "Twilight Town",
-      "Hollow Bastion",
-      "Castle Oblivion",
-      "Land of Departures"
-    ]
+    optionA:"Twilight Town",
+    optionB:"Hollow Bastion",
+    optionC:"Castle Oblivion",
+    optionD:"Land of Departures"
+    
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
     {
-    numb: 6,
     question: "Who is 'the king?'",
     answer: "Mickey",
-    options: [
-      "Mickey",
-      "Sora",
-      "Xemnas",
-      "Pete"
-    ]
+    optionA:"Mickey",
+    optionB:"Sora",
+    optionC:"Xemnas",
+    optionD:"Pete"
+    
   },
-
   {
-    numb: 7,
     question: "What is Hollow Bastion's original name?",
-    answer: "Radian Garden",
-    options: [
-      "Pride Lands",
-      "Deep Jungle",
-      "Disney Springs",
-      "Magic Kingdom"
-    ]
+    answer: "Radiant Garden",
+    optionA:"Pride Lands",
+    optionB:"Deep Jungle",
+    optionC:"Disney Springs",
+    optionD:"Magic Kingdom"
+    
   },
-
   {
-    numb: 8,
     question: "Who turns into a heartless in the first game?",
     answer: "Sora",
-    options: [
-      "Riku",
-      "Kairi",
-      "Sora",
-      "Goofy"
-    ]
+    optionA:"Riku",
+    optionB:"Kairi",
+    optionC:"Sora",
+    optionD:"Goofy"
+    
   },
-
   {
-    numb: 9,
     question: "On what platform did the second game, Chain of Memories, come out?",
-    answer: "Game Boy",
-    options: [
-      "Nintendo DS",
-      "Play Station",
-      "Game Boy",
-      "Sega Dreamcast"
-    ]
+    answer: "Game Boy", 
+    optionA:"Nintendo DS",
+    optionB:"PlayStation",
+    optionC:"Game Boy",
+    optionD:"Sega Dreamcast"
+    
   },
-
   {
-    numb: 10,
     question: "Who is the main villain in Kingdom Hearts?",
     answer: "Xehanort",
-    options: [
-      "Xigbar",
-      "Xemnas",
-      "Axel",
-      "Xehanort"
-    ]
-  },
-];
-
-// var userAns = answer.textContent; //getting user selected option
-// var correctAns = questions[que_count].answer;
-
-
-// function playing() {
-//     if (userAns === correctAns {
-//         /// Add 2 point to scores, show dialogue that it is correct
-//         console.log()
-//     } else { 
-//         /// Deduct time, show dialogue that answer was incorrect
-//     }
+    optionA:"Xigbar",
+    optionB:"Xemnas",
+    optionC:"Axel",
+    optionD:"Xehanort"
     
-// }
-
-
-
-
-
-var timer; 
-var timeLeft = 3; // seconds
-
-
-// When the timer runs, replace timer and information with game over 
-function gameOver() {   
-    gameOverScrn.style.display='block';
-    playAgainBtn.style.display='block';
-    countdown.style.display='none';
-    info_box.style.display='none';
-    youLost.style.display='block';
-
-
-}
-
-
-function youWin() {
-
-}
-
-
-function updateTimer() {
-  timeLeft = timeLeft - 1;
-  if(timeLeft >= 0) {
-     countdown.innerHTML = timeLeft;
-  } else {
-      gameOver();
-
-        // Play again button reloads page
-    document.querySelector('#play-again').addEventListener('click', function(){
-    window.location.reload();
-    return false;
-  });
-
-    ;
-  }
-}
-
-// The button has an on-click event handler that calls this
-function start() {
-  // setInterval is a built-in function that will call the given function
-  // every N milliseconds (1 second = 1000 ms)
-    startBtn.addEventListener("click", function() {
-    timer = setInterval(updateTimer, 1000);
+    },
+  ];
   
-    // It will be a whole second before the time changes, so we'll call the update
-    // once ourselves
-    updateTimer();
+  
+  
+  
+  
+//   Initial screen
+  firstScreen = document.querySelector("#first")
+  startBtn = document.querySelector("#start");
+  playAgainBtn = document.querySelector("#play-again");
+  gameOverScrn = document.querySelector("#GameOver");
+  clock = document.querySelector("#countdown")
+  info_box = document.querySelector(".info_box");
+  youLost = document.querySelector("#youlost");
+  timerEl = document.querySelector("#timer");
+  
+// Question screen 
+
+
+
+  
+  
+  
+  function gameOver() {
+    playAgainBtn.style.display="block";
+    timerEl.style.display="none";
+    info_box.style.display="none";
+    youLost.style.display="block";
+    clock.style.display="none";
+    startBtn.style.display="none";  
+    questionScreen.style.display="none";
+    firstScreen.style.display="block";
+    gameOverScrn.style.display="block";
+
+  
+    document.querySelector('#play-again').addEventListener('click', function(){
+      window.location.reload();
+       return false;
+    });
+  
+  }
+  
+  // function youWin() {
+  
+  // }
+  
+  function countdownTimer() {
+      var timeLeft = 5;
+  
+      // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+        setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+          // Set the `textContent` of `timerEl` to show the remaining seconds
+          timerEl.textContent = timeLeft;
+          // Decrement `timeLeft` by 1
+          timeLeft--;
+        } else if (timeLeft === 1) {
+          // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+          timerEl.textContent = timeLeft;
+          timeLeft--;
+        } else {
+          // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+          gameOver();
+         
+          
+          // Use `clearInterval()` to stop the timer
+          // Call the `displayMessage()` function
+        }
+      }, 1000);
+  
+  }
+  
+  function starting() {
+    countdownTimer();
+    firstScreen.style.display="none";
+    questionScreen.style.display="block";
+    // startQuiz();
+
+
+  
+      
+  }
+  
+  
+  
+  startBtn.addEventListener("click", function() {
+    starting();
+   
+  }
+  )
+
+
+
+
+//   nextBtn.addEventListener("click", function() {
+
+//  
+
+
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+  
+
+  
+
+
+
+
+// Populating questions into html
+
+// Variables
+questionScreen = document.querySelector("#game-quiz")
+questionNum = document.querySelector("#question-number");
+score = document.querySelector("#player-score");
+question = document.querySelector(".question")
+A = document.querySelector("#A-label")
+B = document.querySelector("#B-label")
+C = document.querySelector("#C-label")
+D = document.querySelector("#D-label")
+
+aInput = document.querySelector("#A")
+bInput = document.querySelector("#B")
+cInput = document.querySelector("#C")
+dInput = document.querySelector("#D")
+nextBtn = document.querySelector(".next")
+
+
+
+
+
+for (var i = 0; i < quiz.length; i++) {
+    // POPULATE SELECT ELEMENT WITH JSON.
+      question.innerHTML =
+        quiz[i]['question']
+
+        A.innerText = quiz[i]['optionA'];
+        B.innerText = quiz[i]['optionB'];
+        C.innerText = quiz[i]['optionC'];
+        D.innerText = quiz[i]['optionD'];
+
+
+
+
+
+        // if (aInput.checked.innerHTML == quiz[i]['answer']) {
+        //     console.log("right answer")
+        
+        // } else {
+        //     console.log("wrong")
+        // }
+        
 
     
-    // We don't want the to be able to restart the timer while it is running,
-    // so hide the button.
-      startBtn.style.display='none';
+};
+
+nextBtn.addEventListener("click", function() {
+
+})
 
 
-
-  })
-
-
+function submit() {
 
 }
 
@@ -233,7 +262,6 @@ function start() {
 
 
 
-
-
-
-
+  
+  
+// Setting the question text
